4077688d8f703545247b2a9ea11d9279949eefa1
ich <xhyxx25@gmail.com>
2025-08-18 23:34:46 +0800

    4.9

--- themes/gitbook/components/SocialButton.js
+++ themes/gitbook/components/SocialButton.js
@@ -1,6 +1,6 @@
 import QrCode from '@/components/QrCode'
 import { siteConfig } from '@/lib/config'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { handleEmailClick } from '@/lib/plugins/mailEncrypt'
 
 /**
@@ -81,7 +81,7 @@ const SocialButton = () => {
             rel='noreferrer'
             title={'weibo'}
             href={CONTACT_WEIBO}>
-            <i className='transform hover:scale-125 duration-150 fab fa-weibo dark:hover:text-green-400 hover:text-green-600' />
+            <i className='fab fa-weibo dark:hover:text-green-400 hover:text-green-600' />
           </a>
         )}
         {CONTACT_INSTAGRAM && (
