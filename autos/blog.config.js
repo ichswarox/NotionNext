7b3e3d39d71102be037a834a506b9188185b8c91
ich <xhyxx25@gmail.com>
2025-09-04 00:09:51 +0800

    app page

--- autos/blog.config.js
+++ autos/blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
-    '253acfe061744620a605b12cc41915e2,en:253acfe061744620a605b12cc41915e2',
+    '2621baa93282808b95a2cbbcd3ccfdcb,en:2621baa93282808b95a2cbbcd3ccfdcb',
   THEME: process.env.NEXT_PUBLIC_THEME || 'gitbook', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2021, // e.g if leave this empty, current year will be used.
