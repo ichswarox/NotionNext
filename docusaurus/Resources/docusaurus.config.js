30fd8bf0da624fd7bcb4be2cd1ae8ecbfa98d1bd
ich <xhyxx25@gmail.com>
2025-09-12 12:44:35 +0800

    Update docusaurus.config.js

--- docusaurus/Resources/docusaurus.config.js
+++ docusaurus/Resources/docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Resourcesd52dd2',
+  title: 'Resourcesd52dddd2',
   tagline: 'Download Software, 3D Models, Video Templates, Music and More',
   favicon: 'img/favicon.ico',
 
