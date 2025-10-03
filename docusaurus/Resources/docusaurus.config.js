5c1b33d1630d7d7104b153fee6868ddd8b445033
ich <xhyxx25@gmail.com>
2025-10-04 01:17:48 +0800

    Update docusaurus.config.js

--- docusaurus/Resources/docusaurus.config.js
+++ docusaurus/Resources/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://www.ucards.store',
+  url: 'https://resources.ucards.store',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
